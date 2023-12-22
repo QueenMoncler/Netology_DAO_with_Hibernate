b080521d219e373bca58be3ce909f94e3a883998
queen <Queenmoncler@gmail.com>
2023-12-22 13:18:42 +0300

    Initial commit

---
+++ src/test/java/com/example/netology_dao_with_hibernate/NetologyDaoWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netology_dao_with_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetologyDaoWithHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
